b274840b981f122236b593333cfe669907b7e0b6
habibahh343-dot <habibahh343@gmail.com>
2026-06-05 12:09:56 +0700

    komitmen pertama

--- routes/web.php
+++ routes/web.php
@@ -10,21 +10,22 @@ Route::get('/home', function () {
     return redirect('/');
 })->middleware('auth');
 
-Route::resource('perkiraan', App\Http\Controllers\PerkiraanController::class);
-Route::resource('jurnal', App\Http\Controllers\JurnalUmumController::class);
-Route::resource('pelanggan', App\Http\Controllers\PelangganController::class);
-Route::resource('pemasok', App\Http\Controllers\PemasokController::class);
-Route::resource('faktur_penjualan', App\Http\Controllers\FakturPenjualanController::class);
-Route::resource('faktur_pembelian', App\Http\Controllers\FakturPembelianController::class);
-Route::resource('pembayaran', App\Http\Controllers\PembayaranController::class);
-Route::resource('neraca_saldo', App\Http\Controllers\NeracaSaldoController::class);
-Route::resource('jurnal_detail', App\Http\Controllers\JurnalDetailController::class);
-Route::resource('buku_besar', App\Http\Controllers\BukuBesarController::class);
-Route::resource('saldo_akun', App\Http\Controllers\SaldoAkunController::class);
-Route::resource('laporan_keuangan', App\Http\Controllers\LaporanKeuanganController::class);
-
-// Profile
+// Semua route resource dilindungi auth
 Route::middleware(['auth'])->group(function () {
+    Route::resource('perkiraan', App\Http\Controllers\PerkiraanController::class);
+    Route::resource('jurnal', App\Http\Controllers\JurnalUmumController::class);
+    Route::resource('pelanggan', App\Http\Controllers\PelangganController::class);
+    Route::resource('pemasok', App\Http\Controllers\PemasokController::class);
+    Route::resource('faktur_penjualan', App\Http\Controllers\FakturPenjualanController::class);
+    Route::resource('faktur_pembelian', App\Http\Controllers\FakturPembelianController::class);
+    Route::resource('pembayaran', App\Http\Controllers\PembayaranController::class);
+    Route::resource('neraca_saldo', App\Http\Controllers\NeracaSaldoController::class);
+    Route::resource('jurnal_detail', App\Http\Controllers\JurnalDetailController::class);
+    Route::resource('buku_besar', App\Http\Controllers\BukuBesarController::class);
+    Route::resource('saldo_akun', App\Http\Controllers\SaldoAkunController::class);
+    Route::resource('laporan_keuangan', App\Http\Controllers\LaporanKeuanganController::class);
+
+    // Profile
     Route::get('/profile', function () {
         return view('profile.show');
     })->name('profile.show');
